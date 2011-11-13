cd0ee10be5636cb37febbf6fa3bfbcd88f435182
yagi <sylc.yagi@gmail.com>
2011-11-14 02:06:28 +0900

    build.xmlを追加。jar化できるようにした

--- market-statistics.sh
+++ market-statistics.sh
@@ -1,1 +1,2 @@
-java -classpath ./bin jp.dip.sys1.market.statistics.Main 
+libs=libs/jcommon-1.0.16.jar:libs/jfreechart-1.0.13.jar:libs/spiffy-with_source-all-0.05.jar:libs/SuperCSV-with_src-1.52.jar
+java -classpath ./market-statistics.jar:$libs jp.dip.sys1.market.statistics.Main 
