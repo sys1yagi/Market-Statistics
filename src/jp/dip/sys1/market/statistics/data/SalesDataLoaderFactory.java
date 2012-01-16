4eda7494b2d2e8b521231bbdb0cd8693dfa05065
yagi <sylc.yagi@gmail.com>
2012-01-16 20:19:03 +0900

    不要なファイルを削除

--- src/jp/dip/sys1/market/statistics/data/SalesDataLoaderFactory.java
+++ src/jp/dip/sys1/market/statistics/data/SalesDataLoaderFactory.java
@@ -1,11 +1,17 @@
 package jp.dip.sys1.market.statistics.data;
 
 /**
- * 
+ * データの読み込むSalesDataLoaderを生成するファクトリクラスです
  * @author sys1yagi
  *
  */
 public class SalesDataLoaderFactory {   
+    
+    /**
+     * settings.propertiesに指定したloader_typeの値によって返却するSalesLoaderが異なります
+     * @param settings
+     * @return
+     */
     public static SalesDataLoader createLoader(Settings settings){
         
         //TODO add settings
--- src/jp/dip/sys1/market/statistics/data/Settings.java
+++ src/jp/dip/sys1/market/statistics/data/Settings.java
@@ -3,7 +3,6 @@ package jp.dip.sys1.market.statistics.data;
 import java.io.Closeable;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Properties;
